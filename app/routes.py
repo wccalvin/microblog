0e77813743891b0c0e76b71ec721b5d1d0691a9b
Clayton William <clayton.calvin@gmail.com>
2019-04-03 06:28:34 -0400

    conditional and loop statements

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,19 @@
 from flask import render_template
 from app import app
 
+
 @app.route('/')
 @app.route('/index')
 def index():
     user = {'username': 'clayton'}
-    return render_template('index.html', title='Home', user=user)
+    posts = [
+        {
+            'author': {'username': 'clayton'},
+            'title': 'Bike path in coventry'
+        },
+        {
+            'author': {'username': 'clayton'},
+            'title': 'Thanos'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
